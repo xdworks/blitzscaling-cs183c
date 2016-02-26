c7aea0ba683a46a642136e122c9255d06da1b993
xdite <xuite.joke@gmail.com>
2016-02-27 00:26:24 +0800

    init

---
+++ cv.sh
@@ -0,0 +1,19 @@
+
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 01- Overview of the Five Stages of Blitzscaling.srt" > 01.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 02- Sam Altman on Y Combinator and What Makes The Best Founders.srt" > 02.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 03- Michael Dearing on Capitalism, Creativity, and Creative Destruction.srt" > 03.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 04- Ann Miura-Ko on FLOODGATE's Thunder Lizard Theory and Achieving Product Market Fit.srt" > 04.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 05- John Lilly on Leveraging Community to Scale Mozilla.srt" > 05.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 06- Jennifer Pahlka on Founding Code For America and Starting the US Digital Service.srt" > 06.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 07- Mariam Naficy on Lessons From The Dot Com Days and Knowing When To Blitzscale.srt" > 07.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 08- Eric Schmidt on Structuring Teams and Scaling Google.srt" > 08.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 10- Selina Tobaccowala on Building a Global Business at SurveyMonkey.srt" > 10.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 11- Patrick Collison on Hiring at Stripe and the Role of a Product-Focused CEO.srt" > 11.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 12- Nirav Tolia on Growing Nextdoor and the Path to Monetization.srt" > 12.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 13- Shishir Mehrotra on Scaling YouTube and The 10 Things That Matter.srt" > 13.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 14- Elizabeth Holmes on Managing Product Strategy, Regulation, and the Media.srt" > 14.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 15- Diane Greene on Scaling Products and Culture At VMware.srt" > 15.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 16- Reed Hastings on Building a Streaming Empire.srt" > 16.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 17- Marissa Mayer on Scaling Google and Yahoo.srt" > 17.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 18- Brian Chesky on Launching Airbnb and the Challenges of Scale.srt" > 18.md
+sed -r '/^[0-9]+$/{N;d}' "[DownSub.com] Blitzscaling 19- Jeff Weiner on Establishing a Plan and Culture for Scaling.srt" > 19.md
